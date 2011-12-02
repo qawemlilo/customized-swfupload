8bf6ad3b3ca0b348d5b03b19795f7cb7d79fae41
Qawelesizwe <qawemlilo@gmail.com>
2011-12-02 15:27:03 +0200

    Cleaned out some unwanted files and code

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head>
 <title>SWFUpload Demos - Simple Demo</title>
-<link href="../css/default.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="../swfupload/swfupload.js"></script>
-<script type="text/javascript" src="js/swfupload.queue.js"></script>
+<link href="css/default.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="swfupload/swfupload.js"></script>
 <script type="text/javascript" src="js/fileprogress.js"></script>
 <script type="text/javascript" src="js/handlers.js"></script>
 <script type="text/javascript">
-		var swfu;
 
-		window.onload = function() {
-			var settings = {
-				flash_url : "../swfupload/swfupload.swf",
+(function (){
+    var swfu;
+	
+	window.onload = function () {
+	    var settings = {
+		    flash_url : "swfupload/swfupload.swf",
 				upload_url: "upload.php",
 				post_params: {"PHPSESSID" : "<?php echo session_id(); ?>"},
 				file_size_limit : "100 MB",
@@ -28,14 +29,11 @@
 
 				// Button settings
 				button_image_url: "images/TestImageNoText_65x29.png",
-				button_width: "65",
-				button_height: "29",
+				button_width: "100",
+				button_height: "30",
+				button_window_mode: SWFUpload.WINDOW_MODE.TRANSPARENT,
 				button_placeholder_id: "spanButtonPlaceHolder",
-				button_text: '<span class="theFont">Hello</span>',
-				button_text_style: ".theFont { font-size: 16; }",
-				button_text_left_padding: 12,
-				button_text_top_padding: 3,
-				
+				button_text: '<span class="theFont">Upload</span>',
 				// The event handler functions are defined in handlers.js
 				file_queued_handler : fileQueued,
 				file_queue_error_handler : fileQueueError,
@@ -48,31 +46,39 @@
 				queue_complete_handler : queueComplete	// Queue plugin event
 			};
 
-			swfu = new SWFUpload(settings);
-	     };
-	</script>
+		swfu = new SWFUpload(settings);
+	};
+}());
+</script>
 </head>
-<body>
-<div id="header">
-	<h1 id="logo"><a href="../">SWFUpload</a></h1>
-	<div id="version">v2.2.0</div>
-</div>
-
-<div id="content">
-	<h2>Simple Demo</h2>
-	<form id="form1" action="index.php" method="post" enctype="multipart/form-data">
-		<p>This page demonstrates a simple usage of SWFUpload.  It uses the Queue Plugin to simplify uploading or cancelling all queued files.</p>
-
-			<div class="fieldset flash" id="fsUploadProgress">
-			<span class="legend">Upload Queue</span>
+  <body>
+    <div id="header">
+        <h1 id="logo"><a href="../">SWFUpload</a></h1>
+	    <div id="version">v2.2.0</div>
+    </div>
+  
+    <div id="content">
+        <h2>Simple Demo</h2>
+	
+	    <form id="form1" action="index.php" method="post" enctype="multipart/form-data">
+	        <p>
+			    This page demonstrates a simple usage of SWFUpload.
+			</p>
+			
+			<div class="flash" id="fsUploadProgress">
+			
 			</div>
-		<div id="divStatus">0 Files Uploaded</div>
+			
+			<div id="divStatus">
+			    0 Files Uploaded
+			</div>
+			
 			<div>
-				<span id="spanButtonPlaceHolder"></span>
-				<input id="btnCancel" type="button" value="Cancel All Uploads" onclick="swfu.cancelQueue();" disabled="disabled" style="margin-left: 2px; font-size: 8pt; height: 29px;" />
+			    <span id="spanButtonPlaceHolder"></span>
+				<input type="button" value="Upload Files"id="undercover" />
+				<input id="btnCancel" type="button" value="Cancel All Uploads" onclick="swfu.cancelQueue();" disabled="disabled" />
 			</div>
-
-	</form>
-</div>
-</body>
+		</form>
+    </div>
+  </body>
 </html>
