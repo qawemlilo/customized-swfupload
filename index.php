95e3e31b59e5fb579364107239ef82e2ef0b5c94
Qawelesizwe <qawemlilo@gmail.com>
2011-12-05 15:27:57 +0200

    SWFUpload event handlers created

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <title>SWFUpload Demos - Simple Demo</title>
 <link href="css/default.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="swfupload/swfupload.js"></script>
-<script type="text/javascript" src="js/fileprogress.js"></script>
-<script type="text/javascript" src="js/handlers.js"></script>
+<script type="text/javascript" src="swfupload/swfupload.queue.js"></script>
+<script type="text/javascript" src="swfupload/handlers.js"></script>
 <script type="text/javascript">
 (function (){
     var swfu;
@@ -18,11 +18,16 @@
 				file_size_limit : "100 MB",
 				file_types : "*.*",
 				file_types_description : "All Files",
-				file_upload_limit : 5,
+				file_upload_limit : 100,
 				file_queue_limit : 0,
 				custom_settings : {
 					progressTarget : "fsUploadProgress",
-					cancelButtonId : "btnCancel"
+					cancelButtonId : "btnCancel",
+					statusTarget: "statusDiv",
+					progressContainer: "fsUploadProgress",
+                    queuedTotal: "queued_total",
+                    loadedTotal: "num_loaded",
+                    progressBar: "progressBar"					
 				},
 				debug: false,
 
@@ -34,11 +39,11 @@
 				button_placeholder_id: "spanButtonPlaceHolder",
 				button_text: '<span class="theFont">Upload Files</span>',
 				button_cursor : SWFUpload.CURSOR.HAND,
+				
 				// The event handler functions are defined in handlers.js
-				file_queued_handler : fileQueued,
-				file_queue_error_handler : fileQueueError,
 				file_dialog_complete_handler : fileDialogComplete,
 				upload_start_handler : uploadStart,
+				file_queue_error_handler : fileQueueError,								
 				upload_progress_handler : uploadProgress,
 				upload_error_handler : uploadError,
 				upload_success_handler : uploadSuccess,
@@ -62,8 +67,10 @@
 	
 	    <form id="form1" action="index.php" method="post" enctype="multipart/form-data">
 		
-			<div id="counterDiv"><span id="num_loaded">0</span> / <span id="num_total">0</span> Files Uploaded</div>
+		    <!--  The div that holds the total number of queued file and a counter of uploaded files   -->
+			<div id="counterDiv"><span id="num_loaded">0</span> of <span id="queued_total">0</span> Files Uploaded</div>
 			
+			<!--  The div that holds the total number of queued file and a counter of uploaded files   -->
 			<div class="flash" id="fsUploadProgress">
 			    <a href="#cancelUpload"><img id="cancelUpload" src="images/cancel.png" /></a>
 			    <div id="progressBar">
