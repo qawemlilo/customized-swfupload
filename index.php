7d8fdf4b1f88d5e0ea60309a06141b834c0a08e5
Qawelesizwe <qawemlilo@gmail.com>
2011-12-06 11:25:56 +0200

    README updated

--- index.php
+++ index.php
@@ -61,9 +61,14 @@
         <h1 id="logo"><a href="../">SWFUpload</a></h1>
 	    <div id="version">v2.2.0</div>
     </div>
-  
+    
+	<!-- fork me -->
+	<a target="_blank" href="http://github.com/qawemlilo/customized-swfupload">
+	    <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/30f550e0d38ceb6ef5b81500c64d970b7fb0f028/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub">
+	</a> 
+	
     <div id="content">
-        <h2>Customised Simple Demo</h2>
+        <h2>Custom Simple Demo</h2>
 	
 	    <form id="form1" action="index.php" method="post" enctype="multipart/form-data">
 		
